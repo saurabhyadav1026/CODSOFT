14dd17643ff0c7048846dfcfb4ce8acdc59c59f2
Saurabh Yadav <SAURABHYADAV7041916@GMAIL.COM>
2024-10-25 23:11:56 +0530

    Update Student_grade_calculator.java

--- Student_grade_calculator.java
+++ Student_grade_calculator.java
@@ -6,18 +6,24 @@ import java.util.*;
 public class Student_grade_calculator {
 
     // for getting valid integer number
-    static int valid_int(Scanner sc) {
+    static int valid_int(Scanner sc,int t) {
         int i;
         try {
             i = sc.nextInt();
         } catch (Exception e) {
             sc.next();
             System.out.println("enter valid no:  ");
-            return valid_int(sc);
+            return valid_int(sc,t);
         }
 
-       
-        return i;
+       if(i<0){
+        System.out.println("enter valid no:  ");
+        return valid_int(sc,t);
+       }
+       if(t==1&&i>100){System.out.println("Number should be less or equal to 100. Enter again:");
+    return valid_int(sc, t);
+    }
+    return i;
 
     }
 
@@ -25,8 +31,9 @@ public class Student_grade_calculator {
         Scanner sc = new Scanner(System.in);
         System.out.println("\n\n\n\n\n******************************************************************************************");
 System.out.println("\t\t\tStudent Grade Calculator");
-        System.out.println("\n\n hello! Sir/Ma'am, \nEnter your total subject no. :   "); // greetiing msg
-        int n = valid_int(sc);
+        System.out.println("\n\n hello! Sir/Ma'am, \nEnter your total subject no. :   "); // greeting msg
+        int n = valid_int(sc,0);
+        sc.nextLine();
         String[] sub_name = new String[n];
         int[] sub_mark = new int[n];
         int total = 0;
@@ -36,9 +43,11 @@ System.out.println("\t\t\tStudent Grade Calculator");
 System.out.println("\n");
         for (int i = 0; i < n; i++) {
             System.out.print((i + 1) + ". Subject Name: ");
-            sub_name[i] = sc.next();
+            sub_name[i] = sc.nextLine().toUpperCase().trim().replaceAll("\\s+"," ");
+     
             System.out.print("          Marks: ");
-            sub_mark[i] = valid_int(sc);
+            sub_mark[i] = valid_int(sc,1);
+            sc.nextLine();
             total += sub_mark[i];
         }
 
@@ -56,27 +65,31 @@ System.out.println("\n");
             grade = "C";
 
             for (int i=0;i<sub_name.length;i++) {
-                if(sub_name[i].length()<7){
-                    while(sub_name[i].length()!=7){
+                if(sub_name[i].length()<15){
+                    while(sub_name[i].length()!=15){
                         sub_name[i] = sub_name[i] + " ";
                     }
                 }
             }
-        // displaying marksheet
+        // displaying marks sheet
         System.out.println("******************************************************************************************");
-
-        System.out.println("Your result is:    \n\n\n\n\n  |   S.No.\t\t Subject Name \t\t Marks\t  |");
-
+       
+       System.out.println("Your result is:    ");
+        System.out.println("\n\n\t+-----------------------------------------------------------------+ ");
+       System.out.println("\t|   S.No.\t\t Subject Name \t\t\t Marks\t  |");
+      
+      
         for (int i = 0; i < n; i++) {
-            System.out.println("  |   " + (i + 1) + ".\t\t " + sub_name[i] + "\t\t  " + sub_mark[i] + "\t  |   ");
+            System.out.println("\t|   " + (i + 1) + ".\t\t\t " + sub_name[i] + "\t\t  " + sub_mark[i] + "\t  |   ");
 
         }
-           System.out.println("------------------------------------------------------------ ");
-
+           System.out.println("\t| \t\t\t\t\t\t\t\t  |\n\t| \t\t\t\t\t\t\t\t  | ");
+String total_percent=String.format("%.2f",(total *100.0)/ (n*100));
         System.out
-                .println("\n\n\t|\tTotal marks:  "+n*100+"\t|\n\t|\tTotal Marks:  " + total + "\t|\n\t|\tPercentage:  " + (total / n)
-                        + "%\t|\t\n\t|\tYour grade is: " + grade + "\t|   ");
-        sc.close();
+                .println("\t|\t\t\tTotal marks  :  "+n*100+"\t\t\t  |\n\t|\t\t\tTotal Marks  :  " + total + "\t\t\t  |\n\t|\t\t\tPercentage   :  " + (total_percent) + "%\t\t\t  |\t\n\t|\t\t\tYour grade is: " + grade + "\t\t\t  |   ");
+                System.out.println("\t+-----------------------------------------------------------------+ ");
+       
+                sc.close();
         System.out.println("\n\n*************************************    Thank You   *************************************");
 
           
